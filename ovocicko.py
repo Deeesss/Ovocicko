e870dc7c2a2201c269b02e82731802501ba167b9
Deeesss <111076028+Deeesss@users.noreply.github.com>
2023-09-21 11:56:42 +0200

    Update ovocicko.py

--- ovocicko.py
+++ ovocicko.py
@@ -1,23 +1,23 @@
-zelenina = ["zelí", "okurka", "rajče", "paprika", "ředkev", "mrkev", "brokolice"]
-ovoce = ["jablko", "hruška", "pomeranč", "jahoda", "banán", "kiwi", "malina"]
+zelenina = ["kapusta", "uhorka", "paradajka", "paprika", "redkvička", "mrkva", "brokolica"]
+ovoce = ["jablko", "hruška", "pomaranč", "jahoda", "banán", "kiwi", "malina"]
 
 input_count = 0
 odpoved = "ano"
 
 while True:
     if odpoved == "ano":
-        ovocicko = input("Zadej název libovolného ovoce nebo zeleniny: ")
+        ovocicko = input("Zadaj názov lubovolného ovocia alebo zeleniny: ")
         input_count += 1
         if ovocicko in zelenina:
             zoznam = zelenina.index(ovocicko)
-            print(f"Zadal jsi zeleninu")
+            print(f"Zadal si zeleninu")
         elif ovocicko in ovoce:
             zoznam = ovoce.index(ovocicko)
-            print(f"Zadal jsi ovoce")
+            print(f"Zadal si ovocie")
         else:
-            print("Tvoje slovo nemám v seznamu")
+            print("Tvoje slovo nemám v zoznamu")
     else:
-        print(f"Zadal jsi", input_count, "slov", sep=" ")
+        print(f"Zadal si", input_count, "slov", sep=" ")
         break
 
-    odpoved = input("Přeješ si zadat další slovo? (ano/ne) ")
+    odpoved = input("Prajše si zadať ďalšie slovo? (ano/ne) ")
